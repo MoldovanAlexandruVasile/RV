d1456ba03f07789f6b77f3c95b0790eb740986da
Moldovan Alenxadru <moldovan_alexandru97@yahoo.com>
2019-01-13 14:32:59 +0200

    VR Labs

---
+++ Cannon/Assets/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	public float Sensitivity;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
+		float newRotationX = transform.localEulerAngles.x + Input.GetAxis("Mouse Y") * Sensitivity;
+		gameObject.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
+	}
+}
---
+++ Cannon/Assets/cannon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed = 20;
+	public Rigidbody body;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			GameObject instantiatedProjectile = Instantiate(projectile,
+														   transform.position,
+														   transform.rotation)
+				as GameObject;
+
+			Rigidbody instantiatedProjectile2 = instantiatedProjectile.GetComponent<Rigidbody>();
+			instantiatedProjectile2.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+
+		}
+	}
+}
